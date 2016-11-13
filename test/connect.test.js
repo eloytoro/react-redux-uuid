befccc03bb8c9b9629e674e762279644ece4d051
Eloy Toro <eloytoro@gmail.com>
2016-11-13 16:25:18 -0600

    adds test to unmount the component

--- test/connect.test.js
+++ test/connect.test.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { Provider } from 'react-redux';
-import { UUID_KEY, NAME_KEY } from '../src/constants';
+import React from 'react'
+import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
+import { UUID_KEY, NAME_KEY } from '../src/constants'
 import createReducer from '../src/createReducer'
 import connectUUID from '../src/connect'
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers } from 'redux'
 
 
 describe('connect', () => {
@@ -16,43 +16,47 @@ describe('connect', () => {
       fizzbuzz
     })
   })
-  const store = createStore(reducer);
+  const store = createStore(reducer)
 
-  const Component = () => <div />;
+  const Component = () => <div />
 
-  const ConnectedComponent = connectUUID('counter', state => ({ count: state }))(Component);
+  const ConnectedComponent = connectUUID('counter', state => ({ count: state }))(Component)
 
-  const component = mount(
+  const root = mount(
     <Provider store={store}>
       <ConnectedComponent />
     </Provider>
-  ).find(Component);
+  )
+
+  const component = root.find(Component)
+
+  const uuid = component.props().uuid
 
   it('connects the component to the uuid state', () => {
-    const props = component.props();
-    expect(props.count).toBe(1);
-    expect(props.uuid).not.toBeUndefined();
-    expect(store.getState().uuid.counter[props.uuid]).toEqual(1);
-  });
+    const props = component.props()
+    expect(props.count).toBe(1)
+    expect(props.uuid).not.toBeUndefined()
+    expect(store.getState().uuid.counter[uuid]).toEqual(1)
+  })
 
   it('doesn\'t update the component', () => {
-    const uuid = component.props().uuid;
-    expect(component.props().count).toBe(1);
-    store.dispatch({ type: '@', meta: { [UUID_KEY]: 'wrong', [NAME_KEY]: 'counter' } });
-    expect(component.props().count).toBe(1);
-  });
+    expect(component.props().count).toBe(1)
+    store.dispatch({ type: '@', meta: { [UUID_KEY]: 'wrong', [NAME_KEY]: 'counter' } })
+    expect(component.props().count).toBe(1)
+  })
 
   it('updates the component', () => {
-    const uuid = component.props().uuid;
-    expect(component.props().count).toBe(1);
-    store.dispatch({ type: '@', meta: { [UUID_KEY]: uuid, [NAME_KEY]: 'counter' } });
-    expect(component.props().count).toBe(2);
-  });
+    store.dispatch({ type: '@', meta: { [UUID_KEY]: uuid, [NAME_KEY]: 'counter' } })
+    expect(component.props().count).toBe(2)
+  })
 
   it('doesn\'t update the component', () => {
-    const uuid = component.props().uuid;
-    expect(component.props().count).toBe(2);
-    store.dispatch({ type: '@', meta: { [UUID_KEY]: 'wrong', [NAME_KEY]: 'counter' } });
-    expect(component.props().count).toBe(2);
-  });
-});
+    store.dispatch({ type: '@', meta: { [UUID_KEY]: 'wrong', [NAME_KEY]: 'counter' } })
+    expect(component.props().count).toBe(2)
+  })
+
+  it('unmounts the component', () => {
+    root.unmount()
+    expect(store.getState().uuid.counter[uuid]).toBeUndefined()
+  })
+})
